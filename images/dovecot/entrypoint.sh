d0bc912fcd94a8cce972d163182df39f47b2ee3c
Richard Hillmann <richie@project0.de>
2020-07-27 19:53:54 +0200

    Catch race condition not re-creating empty dhparam

--- images/dovecot/entrypoint.sh
+++ images/dovecot/entrypoint.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
-set -e
-test -f /dhparam/dh.pem || openssl dhparam "${DOVECOT_DHPARAM_BIT}" > /dhparam/dh.pem
+#!/bin/bash
+set -euo pipefail
+
+if ! [ -s /dhparam/dh.pem ]; then
+  openssl dhparam "${DOVECOT_DHPARAM_BIT}" > /dhparam/dh.pem
+fi
+
 mkdir -p /mail /var/lib/dovecot /etc/dovecot/sieve /var/log/dovecot
 gomplate --input-dir /_etc/ --output-dir /etc
 chown -R mail:mail /mail /var/lib/dovecot /etc/dovecot/sieve /var/log/dovecot
