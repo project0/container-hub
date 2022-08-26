af2972ec80a059c702157c4ec91b99bbd39ecc21
Richard Hillmann <richard.hillmann@autoscout24.com>
2022-08-26 12:22:12 +0200

    Upgrade roundcoube to 1.6.0

--- images/roundcube/etc/roundcube/config.inc.php
+++ images/roundcube/etc/roundcube/config.inc.php
@@ -3,10 +3,8 @@ $config['des_key'] = '{{ random.AlphaNum 24 }}';
 $config['db_dsnw'] = 'sqlite:////{{ .Env.ROUNDCUBEMAIL_DB_DIR }}/sqlite.db?mode=0646';
 $config['log_driver'] = 'stdout';
 
-$config['default_host'] = 'localhost';
-$config['default_port'] = '143';
-$config['smtp_server'] = 'localhost';
-$config['smtp_port'] = '587';
+$config['imap_host'] = 'localhost:143';
+$config['smtp_host'] = 'localhost:587';
 
 $config['skin'] = 'larry';
 $config['plugins'] = ['archive', 'zipdownload'];
