aab5f21d02bd7497a7037c42d0d807bc3debfbe8
Richard Hillmann <richard.hillmann@traefik.io>
2023-01-02 14:21:31 +0100

    Add more signatures to clamav

--- images/clamav/entrypoint.sh
+++ images/clamav/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+gomplate --input-dir /_etc/ --output-dir /etc
+
 mkdir -p /var/lib/clamav
 chmod 0755 /var/lib/clamav
 chown -R "${CLAMAV_USER}:${CLAMAV_USER}" /var/lib/clamav
@@ -9,6 +11,6 @@ chown -R "${CLAMAV_USER}:${CLAMAV_USER}" /var/lib/clamav
 [ -f /var/lib/main.cvd ] || freshclam --daemon-notify=disabled
 
 # Run the update daemon
-freshclam -d -c 6
+freshclam -d
 
 exec "$@"
